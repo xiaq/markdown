601ffebf3f5525aee462722fca9ab5bfdff43276
Vít Novotný <witiko@gmail.com>
2016-08-14 13:45:37 +0200

    Added unit test framework + `lunamark/` tests inherited from lunamark.
    The following tests were removed as they were tied to the HTML writer:
    
     - case-insensitive-tags.test
     - escape-lt.test
     - nested-divs.test
    
    The following test was removed, as it tests the ability to create line
    breaks by adding white spaces at the end of lines -- something the
    verbatim commands (`\markdownBegin`, `\markdownEnd` et al.) cannot do,
    as documented:
    
     - line-break.test
    
    The following test was removed, as the slimmed down version of lunamark
    does not support the smart typography extension and relies on the TeX
    ligatures instead:
    
     - smart.test

---
+++ tests/test.sh
@@ -0,0 +1,60 @@
+#!/bin/sh
+# Run a test for each testfile passed as an argument. If a testfile does not
+# contain the expected test result, generate one.
+trap 'rm -rf build/' INT
+for TESTFILE; do
+  printf 'Testfile %s\n' $TESTFILE
+  for FORMAT in templates/*/; do
+    printf '  Format %s\n' $FORMAT
+    for TEMPLATE in ${FORMAT}*.tex; do
+      printf '    Template %s\n' $TEMPLATE
+      sed 's/<FILENAME>/test.tex/g' <$FORMAT/COMMANDS |
+      while read COMMAND; do
+        printf '      Command %s\n' "$COMMAND"
+
+        # Set up the testing directory.
+        rm -rf build/ && mkdir build/ &&
+        cp support/* $TESTFILE build/ &&
+        cd build/ &&
+        sed -r '/^\s*<<<\s*$/{x;q}' \
+          <${TESTFILE##*/} >test-setup.tex &&
+        sed -rn '/^\s*<<<\s*$/,/^\s*>>>\s*$/{/^\s*(<<<|>>>)\s*$/!p}' \
+          <${TESTFILE##*/} >test-input.md &&
+        sed -n '/^\s*>>>\s*$/,${/^\s*>>>\s*$/!p}' \
+          <${TESTFILE##*/} >test-expected.log &&
+        sed 's/<TEST-SETUP-FILENAME>/test-setup.tex/g;
+             s/<TEST-INPUT-FILENAME>/test-input.md/g' <../$TEMPLATE |
+          (IFS=; while read -r LINE; do
+            if printf '%s\n' "$LINE" | grep -q '^\s*<TEST-INPUT-VERBATIM>\s*$'; then
+              cat test-input.md
+            else
+              printf '%s\n' "$LINE"
+            fi
+          done) >test.tex &&
+
+        # Run the test, filter the output and concatenate adjacent lines.
+        eval $COMMAND >/dev/null &&
+        sed -nr '/^\s*TEST INPUT BEGIN\s*$/,/^\s*TEST INPUT END\s*$/{
+          /^\s*TEST INPUT (BEGIN|END)\s*$/!H
+          /^\s*TEST INPUT END\s*$/{s/.*//;x;s/\n//g;p}
+        }' <test.log >test-actual.log &&
+
+        # If the testfile does not contain an expected outcome, use the current
+        # outcome and update the testfile.
+        if ! grep -q '^\s*>>>\s*$' <${TESTFILE##*/}; then
+          cp test-actual.log test-expected.log &&
+          (cat ${TESTFILE##*/} && printf '>>>\n' &&
+            cat test-expected.log) >../$TESTFILE &&
+          printf '      Added the expected test outcome to the testfile.\n'
+        fi &&
+
+        # Compare the expected outcome against the actual outcome.
+        diff test-expected.log test-actual.log &&        
+
+        # Clean up the testing directory.
+        cd .. && rm -rf build/ || { printf 'ERROR\n'; exit 1; }
+
+      done || exit $?
+    done
+  done
+done
