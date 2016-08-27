62b85cfaaa940fc1870e3f7b917053a3216fddb1
Vít Novotný <witiko@gmail.com>
2016-08-27 19:30:58 +0200

    Decrease the verbosity of `test.sh` via set -e.

--- tests/test.sh
+++ tests/test.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 # Run a test for each testfile passed as an argument. If a testfile does not
 # contain the expected test result, generate one.
+set -e
 trap 'rm -rf build/' INT
 for TESTFILE; do
   printf 'Testfile %s\n' $TESTFILE
@@ -9,19 +10,20 @@ for TESTFILE; do
     for TEMPLATE in ${FORMAT}*.tex; do
       printf '    Template %s\n' $TEMPLATE
       sed 's/<FILENAME>/test.tex/g' <$FORMAT/COMMANDS |
-      while read COMMAND; do
+      (while read COMMAND; do
         printf '      Command %s\n' "$COMMAND"
 
         # Set up the testing directory.
-        rm -rf build/ && mkdir build/ &&
-        cp support/* $TESTFILE build/ &&
-        cd build/ &&
+        rm -rf build/
+        mkdir build/
+        cp support/* $TESTFILE build/
+        cd build/
         sed -r '/^\s*<<<\s*$/{x;q}' \
-          <${TESTFILE##*/} >test-setup.tex &&
+          <${TESTFILE##*/} >test-setup.tex
         sed -rn '/^\s*<<<\s*$/,/^\s*>>>\s*$/{/^\s*(<<<|>>>)\s*$/!p}' \
-          <${TESTFILE##*/} >test-input.md &&
+          <${TESTFILE##*/} >test-input.md
         sed -n '/^\s*>>>\s*$/,${/^\s*>>>\s*$/!p}' \
-          <${TESTFILE##*/} >test-expected.log &&
+          <${TESTFILE##*/} >test-expected.log
         sed 's/<TEST-SETUP-FILENAME>/test-setup.tex/g;
              s/<TEST-INPUT-FILENAME>/test-input.md/g' <../$TEMPLATE |
           (IFS=; while read -r LINE; do
@@ -30,36 +32,34 @@ for TESTFILE; do
             else
               printf '%s\n' "$LINE"
             fi
-          done) >test.tex &&
+          done) >test.tex
 
         # Run the test, filter the output and concatenate adjacent lines.
-        eval $COMMAND >/dev/null 2>&1 &&
+        eval $COMMAND >/dev/null 2>&1
         sed -nr '/^\s*TEST INPUT BEGIN\s*$/,/^\s*TEST INPUT END\s*$/{
           /^\s*TEST INPUT (BEGIN|END)\s*$/!H
           /^\s*TEST INPUT END\s*$/{s/.*//;x;s/\n//g;p}
-        }' <test.log >test-actual.log &&
+        }' <test.log >test-actual.log
 
         # If the testfile does not contain an expected outcome, use the current
         # outcome and update the testfile.
         if ! grep -q '^\s*>>>\s*$' <${TESTFILE##*/}; then
-          cp test-actual.log test-expected.log &&
-          (cat ${TESTFILE##*/} && printf '>>>\n' &&
-            cat test-expected.log) >../$TESTFILE &&
+          cp test-actual.log test-expected.log
+          (cat ${TESTFILE##*/} && printf '>>>\n'
+            cat test-expected.log) >../$TESTFILE
           printf '      Added the expected test outcome to the testfile.\n'
-        fi &&
+        fi
 
         # Compare the expected outcome against the actual outcome.
-        diff test-expected.log test-actual.log && # \
-#         || (sed -n '1,/^\s*>>>\s*$/p' <${TESTFILE##*/} && 
-#             cat test-actual.log) >../$TESTFILE &&
+        diff test-expected.log test-actual.log # ||
+#          (sed -n '1,/^\s*>>>\s*$/p' <${TESTFILE##*/} && 
+#            cat test-actual.log) >../$TESTFILE
 
         # Clean up the testing directory.
-        cd .. && rm -rf build/ || { printf 'ERROR\n'; exit 1; } # \
-#         && break
+        cd ..
+        rm -rf build/ # && break
 
-      done || exit $? # \
-#       && break
-    done # \
-#     && break
+      done) # && break
+    done # && break
   done
 done
